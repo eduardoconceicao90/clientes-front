d17229bfc4d5d2c41ccaf86d6ae41a741df21713
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-01-18 21:27:11 -0300

    Ajustes no sidebar e configurando routerLinks

--- src/app/template/template.module.ts
+++ src/app/template/template.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 
@@ -11,7 +12,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [
     NavbarComponent,
