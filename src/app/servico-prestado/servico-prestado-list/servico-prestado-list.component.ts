69ce51913927c50c07c144e1d028dee866ee92c0
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-02-02 13:24:54 -0300

    Criando formulario de pesquisa

--- src/app/servico-prestado/servico-prestado-list/servico-prestado-list.component.ts
+++ src/app/servico-prestado/servico-prestado-list/servico-prestado-list.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicoPrestadoListComponent implements OnInit {
 
+  nome?: string;
+  mes?: number;
+  meses: number[] = [1,2,3,4,5,6,7,8,9,10,11,12];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  find() {
+    console.log(this.nome, this.mes)
   }
 
 }
